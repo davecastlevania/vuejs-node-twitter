c09c8e3d430343195443aa0741cc14152b58f0a8
David Castaneda <the.dave.castaneda@gmail.com>
2019-01-16 17:32:00 -0500

    Add Notification Component, Add SignUp Component, Add SignUp to Routes

---
+++ tweet-app/src/components/Notification.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ui message" :class="type">
+      {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification',
+  props: {
+      message: {
+          type: String,
+          required: true,
+      }, 
+      type: {
+          type: String,
+          required: true,
+      },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
